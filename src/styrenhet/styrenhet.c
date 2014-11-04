4f1da9f766759ad22aaee73273a9c0cfa4590023
hannessnogren <hansn314@student.liu.se>
2014-11-04 17:16:05 +0100

    gitignore updated w Atmes Studio, styrenhet src initiated

---
+++ src/styrenhet/styrenhet.c
@@ -0,0 +1,17 @@
+/*
+ * styrenhet.c
+ *
+ * Created: 2014-11-04 15:44:45
+ *  Author: Hannes
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
