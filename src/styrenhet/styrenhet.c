bf86e95f6fdae7f066416ad49c72e77cffc4d781
hannessnogren <hansn314@student.liu.se>
2014-11-06 16:11:51 +0100

    gitignore updated, initiated src styrenhet and sensorenhet

--- src/styrenhet/styrenhet.c
+++ src/styrenhet/styrenhet.c
@@ -6,12 +6,12 @@
  */ 
 
 
+#define F_CPU 16000000UL
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
 
 int main(void)
 {
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+
 }
