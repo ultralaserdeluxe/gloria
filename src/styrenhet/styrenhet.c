3bd12b6e39e663c1c7dd3a5d1446cec184d47917
Alexander Yngve <aleyn573@student.liu.se>
2014-11-07 00:15:01 +0100

    Created a simple echo test with SPI interrupts.

--- src/styrenhet/styrenhet.c
+++ src/styrenhet/styrenhet.c
@@ -1,17 +1,36 @@
-/*
+﻿/*
  * styrenhet.c
  *
- * Created: 2014-11-04 15:44:45
- *  Author: Hannes
+ * Created: 2014-11-06
+ * Description: Main file.
  */ 
 
 
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
+#include "spi.h"
+
+
+ISR(SPI_STC_vect)
+{
+	/* send to pins of PORTA */
+	PORTA = SPDR;
+}
+
 
 int main(void)
 {
+	/* port a = output */ 
+	DDRA = 0xFF; 
+	
+	spi_slave_init();
+	
+	/* enable global interrupts */ 
+	sei();
 
+    while(1)
+    {
+
+    }
 }
