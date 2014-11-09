950a4cdb2258a1ee5a57d93272cd1efe72148010
Alexander Yngve <aleyn573@student.liu.se>
2014-11-09 16:28:55 +0100

    USART routines in separate file

---
+++ src/styrenhet/usart.c
@@ -0,0 +1,39 @@
+﻿/*
+ * usart.c
+ *
+ * Created: 2014-11-09
+ * Description: Functions for working with USART.
+ */ 
+
+
+#include <avr/io.h>
+
+
+void usart_init( void )
+{
+	/* Set baudrate */
+	UBRR1H = 0x00;
+	UBRR1L = 0x00;
+	/* Enable receiver and transmitter */
+	UCSR1B = (0<<RXEN1)|(1<<TXEN1);
+	/* Set frame format: 8data, 2stop bit */
+	UCSR1C = (1<<USBS1)|(3<<UCSZ10);
+}
+
+void usart_transmit( unsigned char data )
+{
+	/* Wait for empty transmit buffer */
+	while ( !( UCSR1A & (1<<UDRE1)) )
+	;
+	/* Put data into buffer, sends the data */
+	UDR1 = data;
+}
+
+unsigned char usart_receive( void )
+{
+	/* Wait for data to be received */
+	while ( !(UCSR1A & (1<<RXC1)) )
+	;
+	/* Get and return received data from buffer */
+	return UDR1;
+}
