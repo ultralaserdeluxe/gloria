df0cbf9b0f136bc5c9b20ce94cf4c85ca1d8ef0b
Alexander Yngve <aleyn573@student.liu.se>
2014-11-14 14:18:13 +0100

    Added functions for motor control.

---
+++ src/styrenhet/motor.h
@@ -0,0 +1,23 @@
+﻿/*
+ * motor.h
+ *
+ * Created: 2014-11-14
+ * Description: Functions for motor control.
+ */ 
+
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+
+typedef enum {FORWARD, BACKWARD, ROTATE_LEFT, ROTATE_RIGHT} direction_t;
+
+
+void motor_init();
+void set_speed(uint8_t speed);
+void set_speed_left(uint8_t speed);
+void set_speed_right(uint8_t speed);
+void set_direction(direction_t direction);
+
+
+#endif /* MOTOR_H_ */
