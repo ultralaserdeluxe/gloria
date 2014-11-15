40f79ebe5b0c62c199c5d4f6015a4fa059320927
mumsaren <denlj069@student.liu.se>
2014-11-15 18:27:26 +0100

    uppdated styrlogik

--- src/huvudenhet/styrlogik/mainthread.py
+++ src/huvudenhet/styrlogik/mainthread.py
@@ -1,4 +1,5 @@
 from sensorthread import *
+import queue
 
  #[start,automotor,autoarm,harpaket,kalibrera]
 commando = [True,True,False,False,True]
@@ -8,44 +9,47 @@ def main():
     #kommer nog behöva fler
     pick_up = False
     put_down = False  
-
-    sensorthread = SensorThread(0.1)
+    
+    commandQueue = queue.Queue()
+    sensorList = []
+    
+    sensorthread = SensorThread(sensorList)
     sensorthread.start()
+
+    while commandQueue.get(
 
     try:
         while True:
-            #start?
-            if commando[0] == True:
-                #autonom motor och inte befinner sig på stoppstation
-                if (commando[1] == True) and not on_stopstation() :
-                    print "autonom motor\n"
-                    if pick_up == False:
-                        #behöver inte styra armen, fortsätt...
-                        #kolla om vi vill styra armen ändå
-                        if commando[2] == False:
-                            #styr arm
-                            print "waiting for arm input...\n"
-                        if put_down == False:
-                            if check_pick_up_right() or check_pick_up_left():
-                                pick_up = True
-                            else:
-                                pick_up = False
-                            if check_put_down_right() or check_put_down_left():
-                                put_down = True
-                            else:
-                                put_down = False
+            #autonom motor och inte befinner sig på stoppstation
+            if (commando[1] == True) and not on_stopstation() :
+                print "autonom motor\n"
+                if pick_up == False:
+                    #behöver inte styra armen, fortsätt...
+                    #kolla om vi vill styra armen ändå
+                    if commando[2] == False:
+                        #styr arm
+                        print "waiting for arm input...\n"
+                    if put_down == False:
+                        if check_pick_up_right() or check_pick_up_left():
+                            pick_up = True
+                        else:
+                            pick_up = False
+                        if check_put_down_right() or check_put_down_left():
+                            put_down = True
+                        else:
+                            put_down = False
                             regulate()
                             drive_forward()
-                        else:
-                            #sätt ned paket
-                            print "putting down package..."                        
                     else:
-                        if commando[2] == False:
-                            #styr arm 
-                            print "waiting for arm input..."
+                        #sätt ned paket
+                        print "putting down package..."                        
                 else:
-                    #manuellt läge
-                    print "waiting for PC input..."
+                    if commando[2] == False:
+                        #styr arm 
+                        print "waiting for arm input..."
+            else:
+                #manuellt läge
+                print "waiting for PC input..."
 
                                             
             print "pick_up? = " + str(pick_up) + "\nput_down? = " + str(put_down)
@@ -120,12 +124,7 @@ def regulate():
 def drive_forward():
     print "keep on truckin..."
 
-#tar en lista med ett binärtal och gör om det till ett heltal
-def binary_to_int(seq):
-    temp = ""
-    for i in seq:
-        temp += str(i)
-    return int(temp,2)    
+   
     
 
 main()
--- src/huvudenhet/styrlogik/sensorthread.py
+++ src/huvudenhet/styrlogik/sensorthread.py
@@ -3,68 +3,58 @@ import threading
 import random
 import time
 import spidev
- 
-line_sensordata = []
-IR_right_sensordata = []
-IR_left_sensordata = []
-
-#spi = spidev.SpiDev()
-#spi.open(0, 1)
 
 boolean = [0,1]
 
 class SensorThread(threading.Thread):
-    def __init__(self, delay):
+    def __init__(self, sensorList, delay):
         threading.Thread.__init__(self)
+        self.sensorList = sensorList
         self.delay = delay
         self.exitFlag = 0
-        
+        #self.spi = spidev.SpiDev()
+        #self.spi.open(0,1)
+
     def run(self):
         print "starting sensorthread" 
         while not self.exitFlag:
-            spi.writebytes([0x0F]) 
-            #time.sleep(self.delay)
-            #data = spi.readbytes(13)
+            #self.spi.writebytes([0x0F]) 
+            #data = self.spi.readbytes(13)
             data = testreadbytes()
-            divide_data(data)
-            #print_sensordata()
+            self.divide_data(data)
             time.sleep(self.delay)
         
     def kill(self, status):
         self.exitFlag = status
 
+    def divide_data(self, data):
+        temp = []
+        counter = 0
+        del self.sensorList[0][1][0:]
+        #lägger till linjesensordatan i sensorList 
+        for i in data[0:88]:
+            temp.append(i)
+            counter+=1
+            if counter == 8:
+                self.sensorList[0][1].append(binary_to_int(temp))
+                counter = 0
+                del temp[0:]
+        #lägger till avståndsensordata i sensorList
+        del self.sensorList[1][1][0:]
+        self.sensorList[1][1].append(binary_to_int(data[88:96]))
+        self.sensorList[1][1].append(binary_to_int(data[96:104]))
+        
+
 def testreadbytes():
-    
     temp = []
     for i in range(0,104):
         temp.append(random.choice(boolean)) 
     return temp
 
-def divide_data(data):
-        
-    del line_sensordata[0:]
-    for i in data[0:88]:
-        line_sensordata.append(i)
-    del IR_right_sensordata[0:]    
-    for i in data[88:96]:
-        IR_right_sensordata.append(i)
-    del IR_left_sensordata[0:]
-    for i in data[96:104]:
-        IR_left_sensordata.append(i)
-
-def print_sensordata():
-    
-    temp = "line_sensordata: "
-    for i in line_sensordata:
+#tar en lista med ett binärtal och gör om det till ett heltal
+def binary_to_int(seq):
+    temp = ""
+    for i in seq:
         temp += str(i)
-    temp += "\nIR_right_sensordata: "
-    for i in IR_right_sensordata:
-        temp += str(i)
-    temp += "\nIR_left_sensordata: "
-    for i in IR_left_sensordata:
-        temp += str(i)
-
-    print temp    
-
-
+    return int(temp,2) 
 
