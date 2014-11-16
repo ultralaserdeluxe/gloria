5e3ae265278804ff219f747f292fc01cafecaeff
martin <marso329@student.liu.se>
2014-11-16 02:22:03 +0100

    moved some python modules around

---
+++ src/huvudenhet/styrlogik/beagleMain.py
@@ -0,0 +1,21 @@
+"""a simple testprogram for the pcThread class that is used to connects to a user and receives commands and sends data back"""
+from pcThread import pcThread
+import Queue
+import time
+commandQueue=Queue.Queue()
+sensorList=[["lineSensor",[0,0,20,0,512,0,0,100,0,0,0]],
+            ["distance",[30,40]],
+            ["armPosition",[1,2,3,4,5,5]],
+            ["errorCodes",["YngveProgrammedMeWrong"]],
+            ["motorSpeed",[50,50]],
+            ["latestCalibration",["0000-00-00-15:00"]],
+            ["autoMotor",[True]],
+            ["autoArm",[False]]]
+pcThreadObject=pcThread(commandQueue,sensorList)
+pcThreadObject.daemon=True
+pcThreadObject.start()
+while True:
+    time.sleep(1)
+    if not commandQueue.empty():
+        print("executing stuff")
+        print(commandQueue.get())
---
+++ src/huvudenhet/styrlogik/pcThread.py
@@ -0,0 +1,123 @@
+"""a thread class that is used on the glorious Gloria storage robot to connect to a pc and receive commands and sends data back to the user"""
+import threading
+import socket
+import select
+import sys
+import os
+class pcThread(threading.Thread):
+    
+    #a constructor for the class
+    def __init__(self,commandQueue,sensorList):
+        self.__s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.__s.bind(('', 1337))
+        self.__commandQueue=commandQueue
+        self.__sensorList=sensorList
+        self.__conn=None
+        self.__addr=None
+        threading.Thread.__init__(self)
+        
+    #when the thread is started this is started
+    def run(self):
+        
+        #waits for a user to connect
+        def waitForConnection():
+            self.__s.listen(1)
+            self.__conn, self.__addr = self.__s.accept()
+            self.__s.setblocking(0)
+        #recevies data from the user, returns a string as described in the designspecification
+        def receiveData():
+            complete_data_set=""
+            while not complete_data_set:
+                readable, writable, exceptional = select.select([self.__conn], [self.__conn], [self.__conn])
+                while self.__conn not in readable:
+                    readable, writable, exceptional = select.select([self.__conn], [self.__conn], [self.__conn])
+                while self.__conn in readable:
+                    data=self.__conn.recv(4096)
+                    complete_data_set=complete_data_set+data
+                    readable, writable, exceptional = select.select([self.__conn], [self.__conn], [self.__conn])
+                    if not data:
+                        break
+            return complete_data_set
+        
+        #converts the sensorlist to a string to send to the user as described in the designspecifikation
+        def checkSubelement(subelement):
+            if subelement.isdigit():
+                return int(subelement)
+            else:
+                if subelement=="True":
+                    return True
+                elif subelement=="False":
+                    return False
+                else:
+                    return subelement
+        def convertSensorList(list):
+            string=""
+            for element in list:
+                if not string:
+                    string=element[0]+"="+str(element[1][0])
+                else:
+                    string=string+";"+element[0]+"="+str(element[1][0])
+                first=True
+                for subElement in element[1]:
+                    if first:
+                        first=False
+                    else:
+                        string=string+","+str(subElement)
+            return string
+        
+        #a simple function that sends data to the user
+        def sendData(data):
+            self.__conn.sendall(data)
+            
+        #a function not used yet but can be used to remotely shutdown the BB
+        def shutDown():
+            os.system("poweroff")
+            
+        #takes in commands from the mainloop and executes them
+        def commandHandler(data):
+            temp=0;
+            if data[0]=="status":
+                sendData(convertSensorList(self.__sensorList))
+            elif data[0]=="motorSpeed" or data[0]=="armPosition" or data[0]=="autoMotor" or data[0]=="autoArm":
+                for i in range(len(self.__sensorList)):
+                    if self.__sensorList[i][0]==data[0]:
+                        for j in range(len(data[1])):
+                            self.__sensorList[i][1][j]=checkSubelement(data[1][j])
+                        temp=i
+                self.__commandQueue.put(self.__sensorList[temp])
+            elif data[0]=="calibrate":
+                self.__commandQueue.put(data)
+            elif data[0]=="start":
+                self.__commandQueue.put(data)
+            else:
+                pass
+            
+        #takes in a received string and converts it into a list of commands that can easily be used
+        def splitCommand(data):
+            commandList=[]
+            data=data.split(";")
+            for element in data:
+                if "=" not in element:
+                    commandList.append([element])
+                else:
+                    tempList=[]
+                    element=element.split("=")
+                    tempList.append(element[0])
+                    temp=[]
+                    element[1]=element[1].split(",")
+                    for element in element[1]:
+                        temp.append(element)
+                    tempList.append(temp)
+                    commandList.append(tempList)
+            return commandList
+        waitForConnection()
+        while True:
+            commands=splitCommand(receiveData())
+            for element in commands:
+                commandHandler(element)
+            
+        
+        
+                
+            
