9cb4c785849c04ee79d23c3c8a39c175e98f454e
Pål Kastman <palka285@student.liu.se>
2014-11-30 19:10:21 +0000

    fixed functions for distance

--- src/huvudenhet/styrlogik/distance.py
+++ src/huvudenhet/styrlogik/distance.py
@@ -1,15 +1,8 @@
 def distance_left(x):
-    z = (x-41.0)/25.0
-    return (0.093*(z**6)) - (0.77*(z**5)) + (2.4*(z**4)) - (3.9*(z**3)) + (4.7*(z**2)) - (7.4*z) + 13
-
-#for x in range(17,110):
-#    print "x = " + str(x) + " distance = " + str(kastman(x))
+    z = (x-49.0)/31.0
+    return (0.14*(z**6))-(0.96*(z**5))+(2.4*(z**4))-(3.2*(z**3))+(4.4*(z**2))-(8.3*z) + 13
 
 
 def distance_right(x):
-    z = (x-40.0)/23.0
-    return (0.83*(z**4)) - (4.2*(z**3)) + (7*(z**2)) - (7.5*z) + 12
-
-#for x in range(1,110):
-#    print "x = " + str(x) + " distance = " + str(kastman_right(x))
-
+    z = (x-50.0)/31.0
+    return (0.041*(z**6))-(0.38*(z**5))+(1.5*(z**4))-(3.3*(z**3))+(5.3*(z**2))-(8.3*z) + 13
