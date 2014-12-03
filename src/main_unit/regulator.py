3dc9a8cac70f81579406fcaa1332ae4b76b957ee
Alexander Yngve <aleyn573@student.liu.se>
2014-12-03 16:11:12 +0100

    Changed to old regulator class but changed position calculations.

--- src/main_unit/regulator.py
+++ src/main_unit/regulator.py
@@ -1,46 +1,122 @@
+import threading
 import time
 
-class Regulator:
-    def __init__(self, sensor_list):
-        self.sensor_list=sensor_list
-        self.sensor_list.append(["regulator", [0, 0]])
-        self.e0 = 0.0
-        self.e1 = 0.0
-        self.e2 = 0.0
-        self.P = 1.0
-        self.I = 1.0
-        self.D = 1.0
-        self.calibration_data = [(74, 198), (127, 210), (150, 220), (50, 184), (140, 226), (65, 180),
-                                 (170, 230), (47, 160), (103, 204), (56, 165), (48, 178)]
-        self.out = 0.0
-        self.last_time = 0
+class Regulator(threading.Thread):
+    
+    #a constructor for the class
+    def __init__(self,sensorList):
+        self.__sensorList=sensorList
+        self.__sensorList.append(["regulator",[0,0]])
+        self.__e0=0.0
+        self.__e1=0.0
+        self.__e2=0.0
+        self.__updateFreq=5.0
+        self.__updateTime=1.0/self.__updateFreq
+        self.__P=1.0
+        self.__I=1.0
+        self.__D=1.0
+        self.__signalOut=0.0
+        self.__filtersig=0.0
+        self.__oldSignalOuts=[]
+        self.calibration_data = [[74,198],[127,210],[150,220],[50,184],[140,226],[65,180],
+                                 [170,230],[47,160],[103,204],[56,165],[48,178]]
         threading.Thread.__init__(self)
-
         
     ##if you change the pid values outside this thread
     def updatePID(self):
-        for i in range(len(self.sensor_list)):
-            if self.sensor_list[i][0] == "PID":
-                self.P = self.sensor_list[i][1][0]
-                self.I = self.sensor_list[i][1][1]
-                self.D = self.sensor_list[i][1][2]
+        for i in range(len(self.__sensorList)):
+            if self.__sensorList[i][0]=="PID":
+                self.__P=self.__sensorList[i][1][0]
+                self.__I=self.__sensorList[i][1][1]
+                self.__D=self.__sensorList[i][1][2]
                 return
         raise SyntaxError("sensor not in list")
     
-    def regulate(self):
-        self.updatePID()
-
-        self.e1 = self.e0
-        self.e0 = self.get_current_error()
-
-        current_time = time.time()
-        delta_seconds = current_time - self.last_time
-        self.last_time = current_time
-
-        self.out = self.P * self.e0 + (self.D / delta_seconds) * (self.e0 - self.e1) 
-
-        self.setMotors()
+    #main function
+    def run(self):
+        while True:
+            #update values if they have changed in sensorlist
+            self.updatePID()
+            #make sure the pid regulators updates with the freq set in constructor
+            time.sleep(self.__updateTime)
+            #time -1 becomes -2
+            self.__e2=self.__e1
+            #time -1 becomes time 0(last error)
+            self.__e1=self.__e0
+            #time 0 becomes the current error
+            self.__e0=self.get_current_error()
+            #the regulation equation see http://portal.ku.edu.tr/~cbasdogan/Courses/Robotics/projects/Discrete_PID.pdf
+            a=self.__P + self.__I * (self.__updateTime/2.0) + self.__D / (self.__updateTime)
+            b=-self.__P + self.__I * (self.__updateTime/2.0)-(2.0*self.__D)/(self.__updateTime)
+            c=self.__D/(self.__updateTime)
+            self.__signalOut=self.__signalOut+a*self.__e0+b*self.__e1+c*self.__e2
+            #restrict the output signal to be to high
+            if self.__signalOut>3.0:
+                self.__signalOut=3.0
+            if self.__signalOut<-3.0:
+                self.__signalOut=-3.0
+            #store the output signal
+            self.__oldSignalOuts.append(self.__signalOut)
+            #calculate the filtered signa
+            self.__filtersig=0.0
+            if len(self.__oldSignalOuts)>=10:
+                while(len(self.__oldSignalOuts)>10):
+                    del self.__oldSignalOuts[0]
+                divider=1.0/1024
+                for i in range(10):
+                    self.__filtersig=self.__filtersig+self.__oldSignalOuts[i]*divider
+                    divider=divider*2
+            #set the motor
+            #if you dont want to use the filtered signal you can use self.setMotors()
+            #self.setMotorsFilter()
+            self.setMotors()
             
+    #set each motorspeed to motorspeed+-motorspeed*signalout
+    def setMotors(self):
+        leftCurrent=self.getLeftMotor()
+        rightCurrent=self.getRightMotor()
+        left=leftCurrent+int(2.0*leftCurrent*self.__signalOut)
+        right=rightCurrent-int(2.0*rightCurrent*self.__signalOut) 
+        self.setRegMotor(left, right)
+    
+    #set each motorspeed to motorspeed+-motorspeed*filteredsignal the multiplicator differes because of different strength in motors, try with the same
+    def setMotorsFilter(self):
+        leftCurrent=self.getLeftMotor()
+        rightCurrent=self.getRightMotor()
+        if self.__filtersig>0:
+            left=leftCurrent+int(4.0*leftCurrent*self.__filtersig)
+            right=rightCurrent-int(2.0*rightCurrent*self.__filtersig)
+        else:
+            left=leftCurrent+int(1.5*leftCurrent*self.__filtersig)
+            right=rightCurrent-int(1.5*rightCurrent*self.__filtersig)
+        self.setRegMotor(left, right)
+    
+    #puts the calculated motorspeeds in the sensorlist
+    def setRegMotor(self,left,right):
+        for i in range(len(self.__sensorList)):
+            if self.__sensorList[i][0]=="regulator":
+                self.__sensorList[i][1][0]=left
+                self.__sensorList[i][1][1]=right
+                return
+        raise SyntaxError("sensor not in list")
+    #returns the leftmotorspeed from the sensorlist
+    def getLeftMotor(self):
+        for i in range(len(self.__sensorList)):
+            if self.__sensorList[i][0]=="motorSpeed":
+                return self.__sensorList[i][1][0]
+        raise SyntaxError("sensor not in list")
+    #returns the rightmotorspeed from the sensorlist
+    def getRightMotor(self):
+        for i in range(len(self.__sensorList)):
+            if self.__sensorList[i][0]=="motorSpeed":
+                return self.__sensorList[i][1][1]
+        raise SyntaxError("sensor not in list")
+    def getSensorValues(self):
+        for i in range(len(self.__sensorList)):
+            if self.__sensorList[i][0]=="lineSensor":
+                return self.__sensorList[i][1]
+        raise SyntaxError("sensor not in list")
+
     def normalize(self, value, minimum, maximum):
         return float(value) / (maximum - minimum)
 
@@ -73,43 +149,3 @@ class Regulator:
         error = calc_error(position)
         
         return error
-
-    #set each motorspeed to motorspeed+-motorspeed*signalout
-    def setMotors(self):
-        leftCurrent=self.getLeftMotor()
-        rightCurrent=self.getRightMotor()
-        left=leftCurrent+int(2.0*leftCurrent*self.out)
-        right=rightCurrent-int(2.0*rightCurrent*self.out) 
-        if self.out>0.0:
-            self.setRegMotor(left, rightCurrent)
-        else:
-            self.setRegMotor(leftCurrent, right)
-
-    #puts the calculated motorspeeds in the sensorlist
-    def setRegMotor(self,left,right):
-        for i in range(len(self.sensor_list)):
-            if self.sensor_list[i][0]=="regulator":
-                self.sensor_list[i][1][0]=left
-                self.sensor_list[i][1][1]=right
-                return
-        raise SyntaxError("sensor not in list")
-
-    #returns the leftmotorspeed from the sensorlist
-    def getLeftMotor(self):
-        for i in range(len(self.sensor_list)):
-            if self.sensor_list[i][0]=="motorSpeed":
-                return self.sensor_list[i][1][0]
-        raise SyntaxError("sensor not in list")
-
-    #returns the rightmotorspeed from the sensorlist
-    def getRightMotor(self):
-        for i in range(len(self.sensor_list)):
-            if self.sensor_list[i][0]=="motorSpeed":
-                return self.sensor_list[i][1][1]
-        raise SyntaxError("sensor not in list")
- 
-    def getSensorValues(self):
-        for i in range(len(self.sensor_list)):
-            if self.sensor_list[i][0]=="lineSensor":
-                return self.sensor_list[i][1]
-        raise SyntaxError("sensor not in list")
