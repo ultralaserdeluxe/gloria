e65251e52697c1a08fc18300576cc64f668dc3ab
hannessnogren <hansn314@student.liu.se>
2014-12-03 21:12:32 +0100

    Made connect bash script more potent

--- src/tools/connect.sh
+++ src/tools/connect.sh
@@ -1,16 +1,59 @@
 #!/bin/bash
+# Script will try to connect to beagleboard over bluetooth
+# If successful, or already connected it will try to open a ssh-session
+# Usage:
+# sudo connect.sh
+# First time invoked will ask for ip to be used. This ip will be saved in connect.conf
+# Saved ip is changed either by changing content of connect.conf or by deleting connect.conf and invoking connect.sh anew
+# Alternative usage:
+# sudo connect.sh <ip-address>
+# Will try to connect with specified ip address. Does not affect saved ip in connect.conf
+main() {
+	local conf_file="./connect.conf"	# Specify conf_file
+	local bt_interface="bnep0"		# Specify BT interface
 
-if [ "$#" -ne 1 ]
-then
-    echo "usage: connect.sh <ip-address>"
-    exit 1
-fi
+	if [ "$#" -ne 1 ]
+	then
+		# No argument - Use conf_file
+		if [ ! -e $conf_file ]
+			then
+			# create conf if doesnt exist
+			echo "Choose an unused ip:"
+			read local ip
+			echo $ip > $conf_file
+		else
+			# else read from file
+			local ip=$(head -n 1 conf_file)
+		fi
+	else
+		# Use argument
+		local ip=$1
+	fi
+	echo Using ip $ip
 
-# Kill connection if it exists
-pand -k 00:19:0E:0F:F0:6F
+	# Check if connected
+	local bluetooth="$(ifconfig | egrep $bt_interface)"
+	if [ -z "${bluetooth}" ]
+	then
+		# If bluetooth not connected, connect
+		echo Connecting...
+		sh ./connect_bt.sh $ip
+	else
+		echo Already connected!
+	fi
 
-# Create new connection
-pand -n -c 00:19:0E:0F:F0:6F
-ifconfig bnep0 $1 up
+	# Check if connected successfully
+	local bluetooth="$(ifconfig | egrep $bt_interface)"
+	if [ -z "${bluetooth}" ]
+	then
+		echo Failed to connect.
+	else
+		# Connect to ssh
+		echo Setting up ssh.
+		ssh ubuntu@192.168.99.1
+		send -- "temppwd\r"
+	fi
+}
 
-exit 0
+main $1
+exit 1
