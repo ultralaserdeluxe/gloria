be8930b8da23594f7ca04f24e46a8e0d016747ac
hannessnogren <hansn314@student.liu.se>
2014-12-11 11:55:37 +0100

    Sensorunit updated to work with additional line sensors

--- src/sensor_unit/sensor_unit.c
+++ src/sensor_unit/sensor_unit.c
@@ -3,7 +3,7 @@
  *
  * Created: 2014-11-04
  * Description: Software for our sensor unit.
- */ 
+ */
 
 #define F_CPU 16000000UL
 #include <avr/io.h>
@@ -18,13 +18,13 @@ sensor_data_t* sensors;
 int main(void)
 {
 	sensors = sensors_init();
-	
+
 	/* Configure SPI */
 	spi_slave_init();
-	
+
 	/* Enable interrupts */
 	sei();
-	
+
 	/* DEBUG */
 	DDRD = 0xFF;
 
@@ -36,32 +36,19 @@ int main(void)
 }
 
 void spi_recieve_handler(unsigned int data)
-{	
+{
 	uint8_t address = data & 0x0F;
-	
+
 	switch(address)
 	{
-		case 0:
-		case 1:
-		case 2:
-		case 3:
-		case 4:
-		case 5:
-		case 6:
-		case 7:
-		case 8:
-		case 9:
-		case 10:
-		SPDR = sensors->line[address];
-		break;
-		case 11:
-		SPDR = sensors->distance[0];
-		break;
 		case 12:
-		SPDR = sensors->distance[1];
-		break;
+			SPDR = sensors->distance[0];
+			break;
+		case 13:
+			SPDR = sensors->distance[1];
+			break;
 		default:
-		SPDR = 0xFF;
-		break;
+			SPDR = sensors->line[address];
+			break;
 	}
 }
