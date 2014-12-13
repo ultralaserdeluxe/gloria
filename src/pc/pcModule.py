8510f7ebf02c1f784fc4922ccd4bd4173009e263
martin <marso329@student.liu.se>
2014-12-13 13:38:17 +0100

    fixed issue that connection did not brake sometimes when bb went down

--- src/pc/pcModule.py
+++ src/pc/pcModule.py
@@ -25,7 +25,11 @@ class pcModule():
                 readable, writable, exceptional = select.select([self.__s], [self.__s], [self.__s])
             if self.__s in readable:
                 while self.__s in readable:
-                    complete_data_set=complete_data_set+self.__s.recv(self.__package_size).decode() #decode MIGHT be needed here
+                    data=self.__s.recv(self.__package_size).decode()
+                    if not data:
+                        print("raised error")
+                        raise socket.error
+                    complete_data_set=complete_data_set+data #decode MIGHT be needed here
                     readable, writable, exceptional = select.select([self.__s], [self.__s], [self.__s])
         return complete_data_set
     
